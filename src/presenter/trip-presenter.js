a7ceedb57ef30a6db984578394d8d706033c425a
Катышевская Олеся Юрьевна <Katyshevskaya-O@gis.lan>
2024-10-26 14:20:07 +0300

    Дополнительная функциональность

---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,45 @@
+import TripView from '../view/trip-view';
+import { RenderPosition } from '../framework/render';
+import { render, replace, remove } from '../framework/render';
+
+export default class TripPresenter {
+  #modelPoints = null;
+  #mainContainer = null;
+  #tripComponent = null;
+
+  constructor({ modelPoints, mainContainer }) {
+    this.#modelPoints = modelPoints;
+    this.#mainContainer = mainContainer;
+
+    this.#modelPoints.addObserver(this.#handleModelEvent);
+  }
+
+  init() {
+    if (this.#modelPoints.points.length === 0) {
+      if (this.#tripComponent !== null) {
+        remove(this.#tripComponent);
+      }
+      return;
+    }
+
+    const prevTripInfoComponent = this.#tripComponent;
+
+    this.#tripComponent = new TripView({
+      points: this.#modelPoints.points,
+      destinations: this.#modelPoints.destinations,
+      offers: this.#modelPoints.offers,
+    });
+
+    if(prevTripInfoComponent === null){
+      render(this.#tripComponent, this.#mainContainer, RenderPosition.AFTERBEGIN);
+      return;
+    }
+
+    replace(this.#tripComponent, prevTripInfoComponent);
+    remove(prevTripInfoComponent);
+  }
+
+  #handleModelEvent = () => {
+    this.init();
+  };
+}
--- src/view/trip-view.js
+++ src/view/trip-view.js
@@ -1,19 +1,108 @@
 import AbstractView from '../framework/view/abstract-view';
+import { humanizePointDate } from '../utils/common';
+import { TRIP_INFO_DATE_FORMAT } from '../const';
 
-function createTripInfoTemplate() {
+const getFirstPoint = (points) => {
+  const sortedByDateFromPoints = [...points].sort((pointA, pointB) => pointA.dateFrom - pointB.dateFrom);
+  const firstPoint = sortedByDateFromPoints[0].dateFrom;
+  return firstPoint;
+};
+
+const getLastPoint = (points) => {
+  const sortedByDateFromPoints = [...points].sort((pointA, pointB) => pointB.dateTo - pointA.dateTo);
+  const lastPoint = sortedByDateFromPoints[0].dateTo;
+  return lastPoint;
+};
+
+const getDestinationsTitle = (points, destinations) => {
+  const sortedByDateFromPoints = [...points].sort((pointA, pointB) => pointA.dateFrom - pointB.dateFrom);
+
+  const firstDestinationId = sortedByDateFromPoints[0].destination;
+  const lastDestinationId = sortedByDateFromPoints[sortedByDateFromPoints.length - 1].destination;
+
+  const firstDestination = destinations.find((destination) => destination.id === firstDestinationId).name;
+  const lastDestination = destinations.find((destination) => destination.id === lastDestinationId).name;
+
+  if (points.length === 1) {
+    return `${firstDestination}`;
+  }
+
+  if (points.length === 2) {
+    const secondDestinationId = sortedByDateFromPoints[1].destination;
+    const secondDestination = destinations.find((destination) => destination.id === secondDestinationId).name;
+    return `${firstDestination} &mdash; ${secondDestination}`;
+  }
+
+  if (points.length === 3) {
+    return `${firstDestination} &mdash; ${lastDestination}`;
+  }
+
+  if (points.length > 3) {
+    return `${firstDestination} &mdash; . . . &mdash; ${lastDestination}`;
+  }
+};
+
+const getAllOffersCollection = (offers) => {
+  const allOffersInfo = offers.map((offer) => offer.offers).flat();
+  const allOffersCollection = new Map();
+
+  allOffersInfo.forEach(() => {
+    allOffersCollection.set(offers.id, offers.price);
+  });
+
+  return allOffersCollection;
+};
+
+const getOffersFullPrice = (points, offers) => {
+  const pointOffersIdList = points.map((point) => point.offers).flat();
+  const allOffersCollection = getAllOffersCollection(offers);
+
+  let offersFullPrice = 0;
+
+  allOffersCollection.forEach((value, key) => {
+    pointOffersIdList.forEach((pointOfferId) => {
+      if (key === pointOfferId) {
+        offersFullPrice += value;
+      }
+    });
+  });
+
+  return offersFullPrice;
+};
+
+const getPointsFullPrice = (points) => {
+  const allBasePriceList = points.map((point) => point.basePrice);
+  const allBasePrice = allBasePriceList.reduce((priceA, priceB) => priceA + priceB, 0);
+
+  return allBasePrice;
+};
+
+function createTripTemplate(points, destinations, offers) {
   return `<section class="trip-main__trip-info  trip-info">
   <div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-    <p class="trip-info__dates">18&nbsp;&mdash;&nbsp;20 Mar</p>
+  <h1 class="trip-info__title">${getDestinationsTitle(points, destinations)}</h1>
+    <p class="trip-info__dates">${humanizePointDate(getFirstPoint(points), TRIP_INFO_DATE_FORMAT)}&nbsp;&mdash;&nbsp;${humanizePointDate(getLastPoint(points), TRIP_INFO_DATE_FORMAT)}</p>
   </div>
+
   <p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${getPointsFullPrice(points) + getOffersFullPrice(points, offers)}</span>
   </p>
-</section>`;
+  </section>`;
 }
 
-export default class TripView extends AbstractView{
+export default class TripView extends AbstractView {
+  #points = [];
+  #destinations = [];
+  #offers = [];
+
+  constructor({ points, destinations, offers }) {
+    super();
+    this.#points = points;
+    this.#destinations = destinations;
+    this.#offers = offers;
+  }
+
   get template() {
-    return createTripInfoTemplate();
+    return createTripTemplate(this.#points, this.#destinations, this.#offers);
   }
 }
